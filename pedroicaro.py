b075159d68e3ae0fd81537160f558e26e3660f8c
icarop16 <pedroicaro161107@gmail.com>
2023-10-11 11:01:59 -0300

    Atividade Antecessor e Sucessor

---
+++ pedroicaro.py
@@ -0,0 +1,6 @@
+#Escreva um numero e veja qual o seu antecessor e seu sucessor!
+num = int(input("Digite um numero:"))
+Antecessor = num-1
+Sucessor = num+1 
+print("o sucessor de ", num, "é:", Sucessor)
+print("o antecessor de", num, "é:", Antecessor)
